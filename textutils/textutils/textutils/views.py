1eca077d21b212cbab741ab19a843e54d130a624
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-02-17 23:42:45 +0530

    how to use urls

--- textutils/textutils/textutils/views.py
+++ textutils/textutils/textutils/views.py
@@ -1,1 +1,9 @@
 # I have created this file - Shivam
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World")
+
+import urllib.request
+def sh(request):
+    return HttpResponse(urllib.request.urlopen("https://github.com/imshivamchoudhary").read())
