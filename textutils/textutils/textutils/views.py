b7ce72843944c5b7fbd6d0c1e63b62d6258fdec3
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-02-17 23:59:40 +0530

    adding pipeline

--- textutils/textutils/textutils/views.py
+++ textutils/textutils/textutils/views.py
@@ -1,9 +1,29 @@
 # I have created this file - Shivam
 from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello World")
-
 import urllib.request
 def sh(request):
     return HttpResponse(urllib.request.urlopen("https://github.com/imshivamchoudhary").read())
+
+def index(request):
+    return HttpResponse("Home")
+
+
+
+def removepunc(request):
+    return HttpResponse("Removed Punctuation")
+
+
+def capfirst(request):
+    return HttpResponse("Capitalized First Letter")
+
+def newlineremove(request):
+    return HttpResponse("Removed Newlines")
+
+
+def spaceremove(request):
+    return HttpResponse("Removed Spaces")
+
+
+def charcount(request):
+    return HttpResponse("Character Count")`
+
