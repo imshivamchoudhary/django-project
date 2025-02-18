f4bd2675c53aca7b6c43d6087d26e71d90c922b5
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-02-18 23:23:54 +0530

    add templates

--- textutils/textutils/textutils/views.py
+++ textutils/textutils/textutils/views.py
@@ -1,29 +1,33 @@
 # I have created this file - Shivam
 from django.http import HttpResponse
+from django.shortcuts import render
+
 import urllib.request
+
 def sh(request):
-    return HttpResponse(urllib.request.urlopen("https://github.com/imshivamchoudhary").read())
+    return HttpResponse(urllib.request.urlopen("google.com").read())
 
 def index(request):
-    return HttpResponse("Home")
+    params = {'name': 'Shivam', 'place': 'India'}
+    return render(request,"index.html",params)
 
 
 
 def removepunc(request):
-    return HttpResponse("Removed Punctuation")
+    return HttpResponse("Removed Punctuation <a href='/'>back</a>")
 
 
 def capfirst(request):
-    return HttpResponse("Capitalized First Letter")
+    return HttpResponse("Capitalized First Letter <a href='/'>back</a>")
 
 def newlineremove(request):
-    return HttpResponse("Removed Newlines")
+    return HttpResponse("Removed Newlines <a href='/'>back</a>")
 
 
 def spaceremove(request):
-    return HttpResponse("Removed Spaces")
+    return HttpResponse("Removed Spaces <a href='/'>back</a>")
 
 
 def charcount(request):
-    return HttpResponse("Character Count")`
+    return HttpResponse("Character Count <a herf='/'>back</a>")
 
