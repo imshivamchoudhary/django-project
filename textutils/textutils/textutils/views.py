dd1aa6190e9677a54092709938b648317db94dbc
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-02-19 23:41:10 +0530

    add new backend code

--- textutils/textutils/textutils/views.py
+++ textutils/textutils/textutils/views.py
@@ -2,10 +2,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-import urllib.request
-
-def sh(request):
-    return HttpResponse(urllib.request.urlopen("google.com").read())
+# import urllib.request
+#
+# def sh(request):
+#     return HttpResponse(urllib.request.urlopen("google.com").read())
 
 def index(request):
     params = {'name': 'Shivam', 'place': 'India'}
@@ -13,25 +13,37 @@ def index(request):
 
 
 
-def removepunc(request):
+def analyze(request):
     #Get the text
     djtext=request.GET.get('text','default')
+    removepunc=request.GET.get('removepunc','off')
+    print(removepunc)
     print(djtext)
-    #Analyze the text
-    return HttpResponse("Removed Punctuation <a href='/'>back</a>")
+    punctuations='''!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'''
+    # analyzed=djtext
+    if removepunc=='on':
+        analyzed=''
+        for char in djtext:
+            if char not in punctuations:
+                analyzed=analyzed+char
+        params={'purpose':'removed punctuations','analyzed_text':analyzed}
+        #Analyze the text
+        return render(request,'analyze.html',params)
+    else:
+        return HttpResponse('error')
 
-
-def capfirst(request):
-    return HttpResponse("Capitalized First Letter <a href='/'>back</a>")
-
-def newlineremove(request):
-    return HttpResponse("Removed Newlines <a href='/'>back</a>")
-
-
-def spaceremove(request):
-    return HttpResponse("Removed Spaces <a href='/'>back</a>")
-
-
-def charcount(request):
-    return HttpResponse("Character Count <a herf='/'>back</a>")
+#
+# def capfirst(request):
+#     return HttpResponse("Capitalized First Letter ")
+#
+# def newlineremove(request):
+#     return HttpResponse("Removed Newlines")
+#
+#
+# def spaceremove(request):
+#     return HttpResponse("Removed Spaces ")
+#
+#
+# def charcount(request):
+#     return HttpResponse("Character Count ")
 
